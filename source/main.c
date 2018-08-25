4f3f0286bb9ce323f5e7300bd11d9cd7840c4b6e
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2018-08-25 20:07:00 +0100

    Minor refactor

--- source/main.c
+++ source/main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <switch.h>
 
+/**
+ * Text colors
+ */
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
@@ -11,7 +14,13 @@
 #define ANSI_COLOR_RESET   "\x1b[0m"
 #define MAX_CELLS 8
 
+/**
+ * Default game settings, probably dont need to change these
+ */
+char* player2 = "O";
+char* player1 = "X";
 char* defaultCell = "-";
+bool gameRunning = true;
 
 char* cells[9] = {
 	"-", "-", "-", 
@@ -21,8 +30,6 @@ char* cells[9] = {
 
 int currentPosition = 0;
 char* currentPlayer = "X";
-char* player1 = "X";
-char* player2 = "O";
 int player1Score = 0;
 int player2Score = 0;
 
@@ -116,6 +123,53 @@ void checkWinner(){
 }
 
 /**
+ * End turn and swap to the other player
+ */
+void changePlayer()
+{
+	if(currentPlayer == player1){
+	    currentPlayer = player2;
+	} else {
+	    currentPlayer = player1;
+	}
+}
+
+void gameUpdate()
+{
+	u64 key = hidKeysDown(CONTROLLER_P1_AUTO);
+
+    // quit the game
+    if (key & KEY_PLUS) gameRunning = false; 
+
+    // move the player position
+    if (key & KEY_RIGHT) currentPosition++;
+    if (key & KEY_LEFT) currentPosition--;
+    if (key & KEY_UP) currentPosition-=3;
+    if (key & KEY_DOWN) currentPosition+=3;
+
+    // ensure current positon is within bounds
+    if(currentPosition < 0) currentPosition = 0;
+    if (currentPosition > MAX_CELLS) currentPosition = MAX_CELLS;
+
+    drawGame();
+
+    // save cell and swap player
+    if (key & KEY_A){
+        // check if the cell is blank, otherwise warn the player it is already taken
+        if(cells[currentPosition] == defaultCell){
+            // place the current players symbol in the blank space
+            cells[currentPosition] = currentPlayer;
+
+            // check if the player won
+            checkWinner();
+            changePlayer();
+        } else {
+            printf(ANSI_COLOR_RED "\x1b[44;2HSpace is already taken! Please choose another." ANSI_COLOR_RESET);  
+        }     
+    }
+}
+
+/**
  * Main app loop
  * 
  * @param  argc 
@@ -130,50 +184,15 @@ int main(int argc, char **argv)
     // Main loop
     while(appletMainLoop())
     {
-        printf("\x1b[2;60HPress PLUS to exit.");
-
         // draw high scores
+        printf("\x1b[2;60HPress PLUS to exit.");
         printf("\x1b[2;2HPlayer X Score: %i", player1Score);
         printf("\x1b[4;2HPlayer O Score: %i", player2Score);
 
         hidScanInput();
-        u64 isKeyDown = hidKeysDown(CONTROLLER_P1_AUTO);
+        gameUpdate();
 
-        // break in order to return to hbmenu
-        if (isKeyDown & KEY_PLUS) break; 
-
-        // move the player position
-        if (isKeyDown & KEY_RIGHT) currentPosition++;
-        if (isKeyDown & KEY_LEFT) currentPosition--;
-        if (isKeyDown & KEY_UP) currentPosition-=3;
-        if (isKeyDown & KEY_DOWN) currentPosition+=3;
-
-        // ensure current positon is within bounds
-        if(currentPosition < 0) currentPosition = 0;
-        if (currentPosition > MAX_CELLS) currentPosition = MAX_CELLS;
-
-        drawGame();
-
-        // save cell and swap player
-        if (isKeyDown & KEY_A){
-            // check if the cell is blank, otherwise warn the player it is already taken
-            if(cells[currentPosition] == defaultCell){
-                // place the current players symbol in the blank space
-                cells[currentPosition] = currentPlayer;
-
-                // check if the player won
-                checkWinner();
-
-                // end turn and swap to the other player
-                if(currentPlayer == player1){
-                    currentPlayer = player2;
-                } else {
-                    currentPlayer = player1;
-                }
-            } else {
-                printf(ANSI_COLOR_RED "\x1b[44;2HSpace is already taken! Please choose another." ANSI_COLOR_RESET);  
-            }     
-        }
+        if(!gameRunning) break;
 
         gfxFlushBuffers();
         gfxSwapBuffers();
